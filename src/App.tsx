7f3bc7fd3c5824a74c652c6365e566f4f41cfa3b
Lucas Gomes Cruz <lucas.cruz@modalgr.io>
2024-11-07 23:23:27 -0300

    feat: including mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { useLanguage } from './contexts/LanguageContext';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { useLanguage } from "./contexts/LanguageContext";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import { Facebook } from "@mui/icons-material";
 
 const App: React.FC = () => {
   const { t } = useTranslation();
@@ -8,10 +16,25 @@ const App: React.FC = () => {
 
   return (
     <div>
-      <h1>{t('welcome')}</h1>
+      <h1>{t("welcome")}</h1>
       <p>Current language: {language}</p>
-      <button onClick={() => changeLanguage('en')}>English</button>
-      <button onClick={() => changeLanguage('pt')}>Português</button>
+      <button onClick={() => changeLanguage("en")}>English</button>
+      <button onClick={() => changeLanguage("pt")}>Português</button>
+      <Button startIcon={<Facebook/>} variant="contained">Hello world</Button>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value="12"
+          label="Age"
+          onChange={() => {}}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
     </div>
   );
 };
